01361b840ba193d685d862d670b1bc4885a34071
Gerome <gerome.pm@gmail.com>
2023-11-23 15:49:33 +0800

    add blog post title and body.

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,8 @@
 
 ActiveRecord::Schema[7.1].define(version: 2023_11_20_183502) do
   create_table "posts", force: :cascade do |t|
+    t.string "title"
+    t.text "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
